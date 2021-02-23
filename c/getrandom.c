6245dc47c4ba8fc3f3a768747147dc70066780d0
burritoman70 <68611602+burritoman70@users.noreply.github.com>
2021-02-23 18:43:46 +0100

    Update getrandom.c

--- c/getrandom.c
+++ c/getrandom.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*A simple program to get random numbers, made for other programs where a RNG may be needed, thought to be used in Linux*/
 int getrandom(void) {
 	FILE *a=fopen("/dev/urandom","r");
 	srandom(fgetc(a));
