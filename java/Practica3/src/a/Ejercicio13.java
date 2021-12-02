c7cf43d4cab36b80251e2bd3ee2a546f5d038e3d
Carlos Corbalan a.k.a BurritoMan <corbalan.carlos420@gmail.com>
2021-12-02 10:52:30 +0100

    Java fucking sucks

---
+++ java/Practica3/src/a/Ejercicio13.java
@@ -0,0 +1,24 @@
+package a;
+import java.util.*;
+
+public class Ejercicio13 {
+	static Scanner scanner=new Scanner(System.in);
+	public static  void main (String args[]) {
+		Ejercicio13 a=new Ejercicio13();
+		a.evaluarDiscriminante();
+	}
+	void evaluarDiscriminante() {
+		double x2,x,number,result1,result2;
+		x2=scanner.nextInt();
+		x=scanner.nextInt();
+		number=scanner.nextInt();
+		if (((x*x)-(4*x2*number))<0) {
+			System.out.print("La ecuación no tiene soluciones reales");
+			scanner.close();
+			return;
+		}
+		result1=(((0-x)+(Math.sqrt((x*x)-(4*x2*number))))/(2*x2));
+		result2=(((0-x)-(Math.sqrt((x*x)-(4*x2*number))))/(2*x2));
+		System.out.print("Las soluciones son "+result1+" y "+result2);
+	}
+}
