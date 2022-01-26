2ee4e704a1b2c49df12987216e4e5150348c51ef
Carlos Corbalan a.k.a BurritoMan <corbalan.carlos420@gmail.com>
2022-01-26 14:04:50 +0100

    Hating java still

---
+++ java/Practica4/src/practica4/Ejercicio1.java
@@ -0,0 +1,8 @@
+package practica4;
+
+public class Ejercicio1 {
+	static int[] array={7,7,7,7,7,7,7,7,7,7};
+	public static void main(String args[]) {
+		for (int i=0;i<10;i++) System.out.print(array[i]);
+	}
+}
