5000b5ffaae7a54f09da92ec36dec390673b6364
cement-muncher <105234427+cement-muncher@users.noreply.github.com>
2022-05-09 19:02:04 +0200

    Delete c directory

--- c/getrandom.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*A simple program to get random numbers, made for other programs where a RNG may be needed, thought to be used in Linux*/
-int getrandom(void) {
-	FILE *a=fopen("/dev/urandom","r");
-	srandom(fgetc(a));
-	return random();
-}
