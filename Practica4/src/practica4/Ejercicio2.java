634b186ec6c7edf94e90b673ce85b49a81dd4efb
cement-muncher <105234427+cement-muncher@users.noreply.github.com>
2022-05-11 19:56:54 +0200

    Add files via upload

---
+++ Practica4/src/practica4/Ejercicio2.java
@@ -0,0 +1,11 @@
+package practica4;
+
+public class Ejercicio2 {
+	public static void main(String args[]) {
+		int []array=new int[20];
+		for (int i=0;i<10;i++) {
+			array[i]=i*5;
+			System.out.print(array[i]+"\n");
+		}
+	}
+}
