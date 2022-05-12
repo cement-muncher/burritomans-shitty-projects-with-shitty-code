0da7687f09fb1ed641bb9211d1cb4b79b725a25d
Cement Mucher <internationalroverpathfinder@gmx.es>
2022-05-12 17:43:08 +0200

    help

--- Practica2/src/a/Ejercicio7.java
+++
@@ -1,9 +0,0 @@
-package a;
-
-public class Ejercicio7 {
-	public static void main(String args[]) {
-		System.out.println("Carlos Corbalan");
-		System.out.println("Calle Gabriel Miró, Murcia");
-		System.out.println("España");
-	}
-}
